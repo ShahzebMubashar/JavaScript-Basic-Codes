f20eb32c1ed760bbae5ed21a3f1d137f88b250d8
GhazanfarSheikh <l226963@lhr.nu.edu.pk>
2024-07-20 19:38:14 +0500

    HAHAHA

--- q1.js
+++ q1.js
@@ -1,15 +1,17 @@
 const objects = [
-    { Fname: 'Abu Bakar', Lname: 'Saddique', value: 10 },
-    { Fname: 'Ali', Lname: 'Raza', value: 15 },
-    { Fname: 'Usman', Lname: 'Umer', value: 5 }
+  { Fname: "Abu Bakar", Lname: "Saddique", value: 10 },
+  { Fname: "Ali", Lname: "Raza", value: 15 },
+  { Fname: "Usman", Lname: "Umer", value: 5 },
 ];
 
 let highestValueObject = objects[0];
 
 for (let i = 1; i < objects.length; i++) {
-    if (objects[i].value > highestValueObject.value) {
-        highestValueObject = objects[i];
-    }
+  if (objects[i].value > highestValueObject.value) {
+    highestValueObject = objects[i];
+  }
 }
 
 console.log(highestValueObject);
+
+// hahahaha a
